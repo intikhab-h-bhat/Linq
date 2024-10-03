3b0164daac8012f4edd3f7bc6432cd5b4ef9bf96
intikhab-h-bhat <intikhabh@gmail.com>
2024-10-03 21:51:23 +0530

    Add project files.

---
+++ Linq/Program.cs
@@ -0,0 +1,16 @@
+﻿
+
+//using System.Linq;
+
+//int[] age = new int[6]
+//List<int> age=new List<int>()
+//{ 12, 10, 15, 9, 22, 7 };
+string[] names = {"Adil","Ali","Intikhab","Ahmad","Mohammad","Abdullah" };
+//var a = from i in age where i > 9 orderby i ascending select i;
+//var a=from name in names where name.Contains("l") select name;
+var a = from name in names where name.StartsWith("A") select name;
+
+foreach (string item in a)
+{
+    Console.WriteLine(item);
+}
