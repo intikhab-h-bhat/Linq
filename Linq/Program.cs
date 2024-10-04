bafeba339a5dac13d40dc1063fccdf0d1dc721b4
intikhab-h-bhat <intikhabh@gmail.com>
2024-10-04 20:40:58 +0530

    linq q types query syntex ,query method anfd mix type

--- Linq/Program.cs
+++ Linq/Program.cs
@@ -3,14 +3,24 @@
 //using System.Linq;
 
 //int[] age = new int[6]
-//List<int> age=new List<int>()
-//{ 12, 10, 15, 9, 22, 7 };
-string[] names = {"Adil","Ali","Intikhab","Ahmad","Mohammad","Abdullah" };
-//var a = from i in age where i > 9 orderby i ascending select i;
-//var a=from name in names where name.Contains("l") select name;
-var a = from name in names where name.StartsWith("A") select name;
+using System.Reflection.Metadata;
 
-foreach (string item in a)
+List<int> age=new List<int>()
+{ 12, 10, 15, 9, 22, 7 };
+//string[] names = {"Adil","Ali","Intikhab","Ahmad","Mohammad","Abdullah" };
+var querySyntex = from obj in age where obj > 9 orderby obj ascending select obj;
+var queryMethod= age.Where(i => i > 9).ToList();
+var queryMixSyntex = (from obj in querySyntex select obj).Sum();
+
+//var a=from name in names where name.Contains("l") select name;
+//var a = from name in names where name.StartsWith("A") select name;
+
+foreach (int item in querySyntex)
 {
-    Console.WriteLine(item);
+    Console.WriteLine($"Linq Query Syntex type: {item}");
 }
+foreach (int item in queryMethod)
+{
+    Console.WriteLine($"Linq Query Method type: {item}");
+}
+Console.WriteLine($"Linq Query Mix Syntex type: {queryMixSyntex}");
