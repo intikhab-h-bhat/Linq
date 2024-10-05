0b6eee1d2cf31478382e60ad78940cccaa018260
intikhab-h-bhat <intikhabh@gmail.com>
2024-10-05 20:24:18 +0530

    using IEnumerable

---
+++ Linq/IenumIqura/EnumerableExample.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Linq.IenumIqura
+{
+    public class EnumerableExample
+    {
+        List<Student> students = new List<Student>()
+        {
+            new Student(){Id=1,Name="Intikhab",Gender="Male"},
+            new Student(){Id=2,Name="Misbah",Gender="Male"},
+            new Student(){Id=3,Name="Maryam",Gender="Female"},
+
+        };
+
+        public void EnumerableMethod()
+        {
+            IEnumerable<Student> students1 = students;
+
+            //var students2 = students1.Where(s => s.Gender == "Male").ToList();
+            var students2 = from stu in students1 where stu.Gender == "Male" select stu;
+            
+            if (students2.Count() > 0)
+            {
+
+                foreach (var student in students2)
+                {
+
+                    Console.WriteLine($"Id:{student.Id} Name: {student.Name} Gender: {student.Gender}");
+                }
+            }
+            else
+            {
+                Console.WriteLine("No student was found");
+            }
+        }
+
+
+
+    }
+
+    public class Student
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Gender { get; set; }
+
+       
+
+    }
+}
--- Linq/Program.cs
+++ Linq/Program.cs
@@ -1,26 +1,31 @@
 ﻿
+using Linq.IenumIqura;
+////using System.Linq;
 
-//using System.Linq;
+////int[] age = new int[6]
+//using System.Reflection.Metadata;
 
-//int[] age = new int[6]
-using System.Reflection.Metadata;
+//List<int> age=new List<int>()
+//{ 12, 10, 15, 9, 22, 7 };
+////string[] names = {"Adil","Ali","Intikhab","Ahmad","Mohammad","Abdullah" };
+//var querySyntex = from obj in age where obj > 9 orderby obj ascending select obj;
+//var queryMethod= age.Where(i => i > 9).ToList();
+//var queryMixSyntex = (from obj in querySyntex select obj).Sum();
 
-List<int> age=new List<int>()
-{ 12, 10, 15, 9, 22, 7 };
-//string[] names = {"Adil","Ali","Intikhab","Ahmad","Mohammad","Abdullah" };
-var querySyntex = from obj in age where obj > 9 orderby obj ascending select obj;
-var queryMethod= age.Where(i => i > 9).ToList();
-var queryMixSyntex = (from obj in querySyntex select obj).Sum();
+////var a=from name in names where name.Contains("l") select name;
+////var a = from name in names where name.StartsWith("A") select name;
 
-//var a=from name in names where name.Contains("l") select name;
-//var a = from name in names where name.StartsWith("A") select name;
+//foreach (int item in querySyntex)
+//{
+//    Console.WriteLine($"Linq Query Syntex type: {item}");
+//}
+//foreach (int item in queryMethod)
+//{
+//    Console.WriteLine($"Linq Query Method type: {item}");
+//}
+//Console.WriteLine($"Linq Query Mix Syntex type: {queryMixSyntex}");
 
-foreach (int item in querySyntex)
-{
-    Console.WriteLine($"Linq Query Syntex type: {item}");
-}
-foreach (int item in queryMethod)
-{
-    Console.WriteLine($"Linq Query Method type: {item}");
-}
-Console.WriteLine($"Linq Query Mix Syntex type: {queryMixSyntex}");
+
+
+EnumerableExample enumerableExample = new EnumerableExample();
+enumerableExample.EnumerableMethod();
