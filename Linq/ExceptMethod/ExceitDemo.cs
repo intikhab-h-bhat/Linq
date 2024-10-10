bf1a32717d1e5b44fd1bdd5928ffa388cef9e8aa
intikhab-h-bhat <intikhabh@gmail.com>
2024-10-10 08:44:28 +0530

    used Except method

---
+++ Linq/ExceptMethod/ExceitDemo.cs
@@ -0,0 +1,105 @@
+﻿using Linq.IenumIqura;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Linq.ExceptMethod
+{
+    public class ExceitDemo
+    {
+
+        public void ExceptWithInt() {
+
+            List<int> list1 = new List<int>() { 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+            List<int> list2 = new List<int>() { 1, 2, 3, 7, 8, 9 };
+
+            IEnumerable<int>  qm = list1.Except(list2).ToList();
+
+            foreach (int i in qm)
+            {
+                Console.WriteLine(i);
+            }
+
+            IEnumerable<int> qs= (from num in list1 select num).Except(list2).ToList();
+            foreach (int i in qs)
+            {
+                Console.WriteLine(i);
+            }
+        }
+        public void ExceptWithString() {
+            string[] string1 = { "Turkey", "Russia", "China", "Canada" };
+            string[] string2 = { "turkey", "Russia", "Canada", "India" };
+        
+            IEnumerable<string> qm= string1.Except(string2).ToList();
+
+            IEnumerable<string> qs= (from c in string1 select c).Except(string2).ToList();
+
+            foreach(string s in qs)
+            {
+                Console.WriteLine(s);
+            }
+        
+        }
+
+        public void ExceptWithStringCase()
+        {
+            string[] string1 = { "Turkey", "Russia", "China", "Canada" };
+            string[] string2 = { "turkey", "Russia", "Canada", "India" };
+
+            IEnumerable<string> qm= string1.Except(string2,StringComparer.OrdinalIgnoreCase).ToList();
+
+            IEnumerable<string> qs = (from c in string1 select c).Except(string2, StringComparer.OrdinalIgnoreCase).ToList();
+
+
+            foreach(var s in qs)
+            {
+                Console.WriteLine(s);
+            }
+        }
+
+
+        public void ExceptWithComlexType() {
+
+            List<Student> allStu = new List<Student>()
+            {
+                new Student(){Id=101,Name="Mohammad"},
+                new Student(){Id=102,Name="Abdullah"},
+                new Student(){Id=103,Name="Aidah"},
+                new Student(){Id=104,Name="Sareena"},
+                new Student(){Id=105,Name="Zoofishan"},
+
+            };
+
+            List<Student> stu6th = new List<Student>()
+            {
+                new Student(){Id=103,Name="Aidah"},
+                new Student(){Id=104,Name="Sareena"},
+                new Student(){Id=105,Name="Zoofishan"},
+
+            };
+
+
+            List<string> qm = allStu.Select(s => s.Name).Except(stu6th.Select(stu=>stu.Name)).ToList();
+
+            List<string> qs = ((from s in allStu select s.Name).Except(stu6th.Select(stu => stu.Name))).ToList();
+
+            //List<string> qs =((from s in allStu select s.Name).Except(from stu in stu6th select stu.Name)).ToList();
+            
+            
+            
+            foreach(var s in qs)
+            {
+                Console.WriteLine(s);
+            }
+
+
+
+
+        }
+
+
+
+    }
+}
--- Linq/Program.cs
+++ Linq/Program.cs
@@ -1,60 +1,15 @@
 ﻿
 using Linq.DistinctDemo;
+using Linq.ExceptMethod;
 using Linq.IenumIqura;
 using Linq.Select_SelectMany;
 using Linq.WhereFilter;
 
 
 
+ExceitDemo exceptDemo = new ExceitDemo();
 
-DistinctDemo distinctDemo = new DistinctDemo();
-//distinctDemo.DistinctTest();
-distinctDemo.IntDistinctDemo();
-
-//WherFilter wherFilter = new WherFilter();
-//wherFilter.WhereFilterDemo();
-
-
-//Select_SelectMany selectDemo=new Select_SelectMany();
-//selectDemo.SelectMultipleProperties();
-//selectDemo.SelectAnonymousObj();
-//selectDemo.RemoveDuplicacy();
-//selectDemo.SelectMethod();
-
-//selectDemo.SelectSingleProperty();
-
-
-
-
-////using System.Linq;
-
-////int[] age = new int[6]
-//using System.Reflection.Metadata;
-
-//List<int> age=new List<int>()
-//{ 12, 10, 15, 9, 22, 7 };
-////string[] names = {"Adil","Ali","Intikhab","Ahmad","Mohammad","Abdullah" };
-//var querySyntex = from obj in age where obj > 9 orderby obj ascending select obj;
-//var queryMethod= age.Where(i => i > 9).ToList();
-//var queryMixSyntex = (from obj in querySyntex select obj).Sum();
-
-////var a=from name in names where name.Contains("l") select name;
-////var a = from name in names where name.StartsWith("A") select name;
-
-//foreach (int item in querySyntex)
-//{
-//    Console.WriteLine($"Linq Query Syntex type: {item}");
-//}
-//foreach (int item in queryMethod)
-//{
-//    Console.WriteLine($"Linq Query Method type: {item}");
-//}
-//Console.WriteLine($"Linq Query Mix Syntex type: {queryMixSyntex}");
-
-
-
-//EnumerableExample enumerableExample = new EnumerableExample();
-//enumerableExample.EnumerableMethod();
-
-//QurableExample qurableExample = new QurableExample();
-//qurableExample.Example();
+//exceptDemo.ExceptWithInt();
+//exceptDemo.ExceptWithString();
+//exceptDemo.ExceptWithStringCase();
+exceptDemo.ExceptWithComlexType();
